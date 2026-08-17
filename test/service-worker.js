9d8f6672e062fdce481b2cfab154dcee5604f1e0
Uran <314028906+UranBajrami@users.noreply.github.com>
2026-08-17 12:55:32 +0200

    Add files via upload

---
+++ test/service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "b1-trainer-v2";
+const CORE_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(names.filter((n) => n !== CACHE_NAME).map((n) => caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Strategie: Netzwerk zuerst (frische Prüfungsvarianten, Wikipedia-Bilder, Supabase-Sync),
+// bei fehlendem Internet zurück auf den Cache -> App bleibt offline nutzbar.
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+
+  // Externe Aufrufe (Supabase, Wikipedia, Sprachausgabe) nicht cachen, nur durchreichen
+  if (url.origin !== self.location.origin) {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
